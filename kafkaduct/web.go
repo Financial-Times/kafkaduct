85f6e7424762589e7f19fa183ae4bd0fe4b32489
Ellis Pritchard <ellis.pritchard@ft.com>
2016-06-14 16:51:55 +0100

    - handle gzip POST bodies - more consistent logging

--- kafkaduct/web.go
+++ kafkaduct/web.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"io/ioutil"
+	"strings"
+	"compress/gzip"
 
 	"github.com/Financial-Times/service-status-go/gtg"
 	"github.com/Financial-Times/service-status-go/httphandlers"
 	"github.com/Shopify/sarama"
 	"github.com/gorilla/mux"
+
+	"github.com/davecgh/go-spew/spew"
+
 )
 
 func StartServer(appConfig *AppConfig) {
@@ -21,7 +25,7 @@ func StartServer(appConfig *AppConfig) {
 
 	protected := root.NewRoute().Headers("X-Api-Key", appConfig.Web.APIKey).Subrouter()
 
-	root.NewRoute().Headers("X-Api-Key", "").HandlerFunc(error(403, "Missing or Invalid Api Key")) // invalid api-key
+	root.NewRoute().Headers("X-Api-Key", "").HandlerFunc(errorFunc(403, "Missing or Invalid Api Key")) // invalid api-key
 
 	registerAPI(protected, appConfig)
 
@@ -29,9 +33,10 @@ func StartServer(appConfig *AppConfig) {
 
 	http.Handle("/", root)
 
-	fmt.Printf("starting server on %s", appConfig.Web.Port)
 
-	logger.Fatalf("%s", http.ListenAndServe(":"+appConfig.Web.Port, root))
+	logger.Printf("starting server on %s", appConfig.Web.Port)
+
+	logger.Fatalf("%s", http.ListenAndServe(":"+appConfig.Web.Port, nil))
 }
 
 func registerAPI(router *mux.Router, appConfig *AppConfig) {
@@ -46,20 +51,54 @@ type service struct {
 	kafkaClient sarama.SyncProducer
 }
 
-func unmarshal(r *http.Request) messageRequest {
+func unmarshal(r *http.Request) (messageRequest, error) {
 	res := messageRequest{}
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
+	defer r.Body.Close()
 
-	json.Unmarshal(buf.Bytes(), &res)
+	var jsonBody []byte
+	var err error
 
-	return res
+	if(strings.Contains(r.Header.Get("Content-Encoding"),"gzip")) {
+		var gz *gzip.Reader
+		gz, err = gzip.NewReader(r.Body)
+
+		if err == nil {
+			defer gz.Close()
+			jsonBody, err = ioutil.ReadAll(gz)
+		}
+
+	} else {
+		jsonBody, err = ioutil.ReadAll(r.Body)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Spew(jsonBody)
+
+	err = json.Unmarshal(jsonBody, &res)
+
+	return res, err
 }
 
 func (s *service) handle(w http.ResponseWriter, r *http.Request) {
 
-	res := unmarshal(r)
+	logger.Printf("Handling write %s %s", r.Host, r.UserAgent())
+
+	res, err := unmarshal(r)
+
+	// Spew(res)
+
+
+	if err != nil  {
+		logger.Printf("Error unmarshalling: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	logger.Printf("Messages %s %d", res.Topic, len(res.Messages))
 
 	for _, element := range res.Messages {
 
@@ -67,7 +106,7 @@ func (s *service) handle(w http.ResponseWriter, r *http.Request) {
 
 		if err1 != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Printf("ERROR Failed to Marshal: %s", err1)
+			logger.Printf("ERROR Failed to Marshal: %s", err1)
 		}
 
 		partition, offset, err := s.kafkaClient.SendMessage(&sarama.ProducerMessage{
@@ -77,9 +116,9 @@ func (s *service) handle(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Printf("ERROR Failed to store your data error=%s", err)
+			logger.Printf("ERROR Failed to store your data error=%s", err)
 		} else {
-			fmt.Printf("Stored with partition=%d offset=%d", partition, offset)
+			logger.Printf("Stored %s with partition=%d offset=%d", element.MessageType, partition, offset)
 		}
 	}
 }
@@ -104,6 +143,10 @@ func goodToGo() gtg.Status {
 	return gtg.Status{GoodToGo: true}
 }
 
-func error(code int, message string) func(http.ResponseWriter, *http.Request) {
+func errorFunc(code int, message string) func(http.ResponseWriter, *http.Request) {
 	return func(resp http.ResponseWriter, req *http.Request) { http.Error(resp, message, code) }
+}
+
+func Spew(a ...interface{}) {
+	spew.Dump(a)
 }
